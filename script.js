64dff37aab0b25b357e85a070e9b5cc8b8f53e7d
luckysahiba <luckysinha2036@gmail.com>
2025-06-13 16:23:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,4 +14,5 @@ async function generateScript() {
       "📝 Description: Watch this amazing shorts on " + topic + "!\n" +
       "📜 Script: Are you ready for " + topic + "? Stay tuned, swipe up!";
   }, 1500);
-}
+}const SUPABASE_URL =https://pliebcpyhnozajwzysog.supabase.co
+const SUPABASE_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsaWViY3B5aG5vemFqd3p5c29nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3OTU0ODYsImV4cCI6MjA2NTM3MTQ4Nn0.z3kYwGJ5jJB4JwbsG8qn-rYd4UzEza5NBCBZaZvcGdA
